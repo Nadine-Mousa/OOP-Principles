3c8387057a8285faaa4ea0809e9f12468f76b517
Nadine Mousa <nadinemousa1@gmail.com>
2025-07-03 10:56:09 +0300

    Encapsulation - Bank Account Example

---
+++ Encapsulation/BankAccount.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Principles.Encapsulation
+{
+    public class BankAccount
+    {
+        private decimal _balance;
+
+        public BankAccount(decimal amount)
+        {
+            Deposit(amount);
+        }
+        // getters
+        public decimal GetBalance()
+        {
+            return this._balance;
+        }
+        // setters
+        public void Deposit(decimal amount)
+        {
+            if(amount < 0)
+            {
+                throw new ArgumentException("Deposit amount must be positive.");
+            }
+            _balance += amount;
+        }
+        public void Withdraw(decimal amount)
+        {
+            if(amount < 0)
+            {
+                throw new ArgumentException("Withdrawal amount must be positive.");
+            }
+            if(amount > _balance)
+            {
+                throw new InvalidOperationException("No sufficient balance");
+            }
+            _balance -= amount;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using OOP_Principles.Encapsulation;
+
+
+
+
+BankAccount mohamedBankAccount = new BankAccount(100);
+
+mohamedBankAccount.Deposit(20);
+Console.WriteLine(mohamedBankAccount.GetBalance());
+
+mohamedBankAccount.Deposit(100);
+Console.WriteLine(mohamedBankAccount.GetBalance());
+
+mohamedBankAccount.Withdraw(50);
+Console.WriteLine(mohamedBankAccount.GetBalance());
+
